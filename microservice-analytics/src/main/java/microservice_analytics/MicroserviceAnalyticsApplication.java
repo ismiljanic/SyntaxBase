06edb615db8dc51867f4a957e17fe1c4f71512b7
ismiljanic <ssmiljanicivan@gmail.com>
2025-12-30 13:58:46 +0100

    Initialize analytics microservice skeleton

---
+++ microservice-analytics/src/main/java/microservice_analytics/MicroserviceAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package microservice_analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceAnalyticsApplication.class, args);
+	}
+
+}
---
+++ microservice-analytics/src/test/java/microservice_analytics/MicroserviceAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package microservice_analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
