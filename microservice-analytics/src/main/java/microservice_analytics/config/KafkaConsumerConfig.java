856f196c826233e7d4772f3696cd50e8ce318e61
ismiljanic <ssmiljanicivan@gmail.com>
2025-12-31 15:30:13 +0100

    Update Kafka consumer configuration class

--- microservice-analytics/src/main/java/microservice_analytics/config/KafkaConsumerConfig.java
+++ microservice-analytics/src/main/java/microservice_analytics/config/KafkaConsumerConfig.java
@@ -1,6 +1,6 @@
 package microservice_analytics.config;
 
-import microservice_analytics.common.EventEnvelope;
+import common.EventEnvelope;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -17,31 +18,30 @@ import java.util.Map;
 public class KafkaConsumerConfig {
 
     @Bean
-    public ConsumerFactory<String, EventEnvelope<?>> consumerFactory() {
-
-        JsonDeserializer<EventEnvelope<?>> deserializer =
-                new JsonDeserializer<>(EventEnvelope.class);
-        deserializer.addTrustedPackages("*");
-
+    public ConsumerFactory<String, EventEnvelope> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); //kafka:9092 for docker
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "analytics-service");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        JsonDeserializer<EventEnvelope> jsonDeserializer = new JsonDeserializer<>(EventEnvelope.class);
+        jsonDeserializer.addTrustedPackages("*");
+        jsonDeserializer.setUseTypeHeaders(false);
+
+        ErrorHandlingDeserializer<EventEnvelope> errorHandlingDeserializer =
+                new ErrorHandlingDeserializer<>(jsonDeserializer);
 
         return new DefaultKafkaConsumerFactory<>(
                 props,
                 new StringDeserializer(),
-                deserializer
+                errorHandlingDeserializer
         );
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, EventEnvelope<?>>
-    kafkaListenerContainerFactory() {
-
-        ConcurrentKafkaListenerContainerFactory<String, EventEnvelope<?>> factory =
+    public ConcurrentKafkaListenerContainerFactory<String, EventEnvelope> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, EventEnvelope> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
-
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
