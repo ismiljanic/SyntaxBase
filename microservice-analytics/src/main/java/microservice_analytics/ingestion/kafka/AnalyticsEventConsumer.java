7ba087150217a4c5e11967261a4c7d23dc080e96
ismiljanic <ssmiljanicivan@gmail.com>
2026-01-02 22:53:51 +0100

    Update consume method to call ingestion service and store to database

--- microservice-analytics/src/main/java/microservice_analytics/ingestion/kafka/AnalyticsEventConsumer.java
+++ microservice-analytics/src/main/java/microservice_analytics/ingestion/kafka/AnalyticsEventConsumer.java
@@ -1,13 +1,19 @@
 package microservice_analytics.ingestion.kafka;
 
 import common.EventEnvelope;
+import microservice_analytics.service.AnalyticsService;
 import org.slf4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnalyticsEventConsumer {
+    private final AnalyticsService ingestionService;
     private static final Logger log = org.slf4j.LoggerFactory.getLogger(AnalyticsEventConsumer.class);
+
+    public AnalyticsEventConsumer(AnalyticsService ingestionService) {
+        this.ingestionService = ingestionService;
+    }
 
     @KafkaListener(
             topics = "chat-events",
@@ -21,5 +27,7 @@ public class AnalyticsEventConsumer {
                 event.getSourceService(),
                 event.getOccurredAt()
         );
+
+        ingestionService.ingest(event);
     }
 }
